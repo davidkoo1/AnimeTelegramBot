3325b63f683634b348952317181563872fa23c3d
davidkoo1 <sverceks@gmail.com>
2024-02-11 20:53:26 +0300

    Get random anime

---
+++ TelegramBot/Program.cs
@@ -0,0 +1,148 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Telegram.Bot;
+using Telegram.Bot.Polling;
+using Telegram.Bot.Types;
+using Telegram.Bot.Types.Enums;
+
+const string token = "6274520148:AAFkH45qrBP5cyc5jSWm5JFM1pIL1MTZbfM";
+
+ITelegramBotClient bot = new TelegramBotClient(token);
+
+Console.WriteLine("Запущен бот " + bot.GetMeAsync().Result);
+
+var cts = new CancellationTokenSource();
+var cancellationToken = cts.Token;
+var receiverOptions = new ReceiverOptions
+{
+    AllowedUpdates = { }, // receive all update types
+};
+bot.StartReceiving(
+    HandleUpdateAsync,
+    HandleErrorAsync,
+    receiverOptions,
+    cancellationToken
+);
+
+Console.ReadLine();
+async Task HandleUpdateAsync(ITelegramBotClient botClient, Update update, CancellationToken cancellationToken)
+{
+    if (update.Message is null && update.CallbackQuery is null)
+        return;
+
+    if (update.Type == Telegram.Bot.Types.Enums.UpdateType.Message)
+    {
+        Console.WriteLine(update.Message.Chat.Id);
+        var message = update.Message.Text.ToLower();
+
+        if (message.StartsWith("/getrandomanime"))
+        {
+            await GetRandomAnime(update);
+        }
+    }
+}
+
+string EscapeMarkdownCharacters(string input)
+{
+    return input.Replace("_", "\\_")
+                .Replace("*", "\\*")
+                .Replace("[", "\\[")
+                .Replace("]", "\\]")
+                .Replace("(", "\\(")
+                .Replace(")", "\\)")
+                .Replace(".", "\\.")
+                .Replace("!", "\\!")
+                .Replace("-", "\\-")
+                .Replace("`", "\\`")
+                .Replace("~", "\\~")
+                .Replace(">", "\\>")
+                .Replace("#", "\\#")
+                .Replace("+", "\\+");
+}
+
+async Task GetRandomAnime(Update update)
+{
+    try
+    {
+        await bot.SendTextMessageAsync(update.Message.Chat.Id, "Wait!!");
+        bool animeConditionsMet = false;
+
+        while (!animeConditionsMet)
+        {
+            // Make a request to the Jikan API to get random anime
+            using (var httpClient = new HttpClient())
+            {
+                try
+                {
+                    var apiUrl = "https://api.jikan.moe/v4/random/anime";
+                    var response = await httpClient.GetAsync(apiUrl);
+
+                    if (response.IsSuccessStatusCode)
+                    {
+                        var data = await response.Content.ReadAsStringAsync();
+
+                        // Использовать JObject для работы с JSON-строкой
+                        JObject json = JObject.Parse(data);
+
+                        // Получить данные напрямую из JSON-строки
+                        string animeTitle = json["data"]["title"]?.ToString() ?? "";
+                        string animeSynopsis = json["data"]["synopsis"]?.ToString() ?? "";
+                        string animeUrl = json["data"]["url"]?.ToString() ?? "";
+                        var animeImageUrl = json["data"]["images"]["jpg"]["image_url"]?.ToString() ?? "";
+
+                        string animeStatus = json["data"]["status"]?.ToString() ?? "";
+                        int animeEpisodes = json["data"]["episodes"] is not null ? Convert.ToInt32(json["data"]["episodes"]) : 0;
+                        string animeType = json["data"]["type"]?.ToString() ?? "";
+                        int animeUserScore = !string.IsNullOrWhiteSpace(json["data"]["scored_by"]?.ToString()) ? Convert.ToInt32(json["data"]["scored_by"]) : 0;
+                        int animeYear = !string.IsNullOrWhiteSpace(json["data"]["year"]?.ToString()) ? Convert.ToInt32(json["data"]["year"]) : 0;
+
+
+
+                        // Check if conditions are met
+                        animeConditionsMet = animeType.ToLower() == "tv"
+                                            && animeEpisodes <= 50 && animeEpisodes > 20 && animeUserScore >= 10000
+                                            && animeStatus.ToLower() == "finished airing";
+
+                        if (animeConditionsMet)
+                        {
+                            // Send the received data as a message
+                            var message = $"*Title:* {EscapeMarkdownCharacters(animeTitle)}\n\n" +
+                                          $"*Synopsis:*\n{EscapeMarkdownCharacters(animeSynopsis)}\n\n" +
+                                          $"*Episodes:*{animeEpisodes}\n\n" +
+                                          $"*Photo:* {EscapeMarkdownCharacters(animeImageUrl)}\n\n";
+                                          
+
+                            var parseMode = Telegram.Bot.Types.Enums.ParseMode.MarkdownV2;
+                            await bot.SendTextMessageAsync(update.Message.Chat.Id, message, parseMode: parseMode);
+                        }
+                        //await Task.Delay(1000);
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    // Handle exceptions, e.g., log or notify the user
+                    Console.WriteLine($"Error getting random anime: {ex.Message}");
+
+                    //await bot.SendTextMessageAsync(update.Message.Chat.Id, "An error occurred while fetching random anime.");
+                }
+            }
+
+            // Introduce a delay of 100 milliseconds before the next iteration
+
+        }
+    }
+    catch (Exception ex)
+    {
+        // Handle exceptions, e.g., log or notify the user
+        Console.WriteLine($"Error getting random anime: {ex.Message}");
+        //await bot.SendTextMessageAsync(update.Message.Chat.Id, "An error occurred while fetching random anime.");
+    }
+}
+
+
+
+async Task HandleErrorAsync(ITelegramBotClient botClient, Exception exception, CancellationToken cancellationToken)
+{
+
+}
---
+++ TelegramBot/Rootobject.cs
@@ -0,0 +1,140 @@
+﻿
+public class Rootobject
+{
+    public Data data { get; set; }
+}
+
+public class Data
+{
+    public int mal_id { get; set; }
+    public string url { get; set; }
+    public Images images { get; set; }
+    public Trailer trailer { get; set; }
+    public bool approved { get; set; }
+    public Title[] titles { get; set; }
+    public string title { get; set; }
+    public object title_english { get; set; }
+    public string title_japanese { get; set; }
+    public string[] title_synonyms { get; set; }
+    public string type { get; set; }
+    public string source { get; set; }
+    public int episodes { get; set; }
+    public string status { get; set; }
+    public bool airing { get; set; }
+    public Aired aired { get; set; }
+    public string duration { get; set; }
+    public string rating { get; set; }
+    public object score { get; set; }
+    public object scored_by { get; set; }
+    public int rank { get; set; }
+    public int popularity { get; set; }
+    public int members { get; set; }
+    public int favorites { get; set; }
+    public object synopsis { get; set; }
+    public object background { get; set; }
+    public object season { get; set; }
+    public object year { get; set; }
+    public Broadcast broadcast { get; set; }
+    public Producer[] producers { get; set; }
+    public object[] licensors { get; set; }
+    public object[] studios { get; set; }
+    public Genre[] genres { get; set; }
+    public object[] explicit_genres { get; set; }
+    public object[] themes { get; set; }
+    public object[] demographics { get; set; }
+}
+
+public class Images
+{
+    public Jpg jpg { get; set; }
+    public Webp webp { get; set; }
+}
+
+public class Jpg
+{
+    public string image_url { get; set; }
+    public string small_image_url { get; set; }
+    public string large_image_url { get; set; }
+}
+
+public class Webp
+{
+    public string image_url { get; set; }
+    public string small_image_url { get; set; }
+    public string large_image_url { get; set; }
+}
+
+public class Trailer
+{
+    public object youtube_id { get; set; }
+    public object url { get; set; }
+    public object embed_url { get; set; }
+    public Images1 images { get; set; }
+}
+
+public class Images1
+{
+    public object image_url { get; set; }
+    public object small_image_url { get; set; }
+    public object medium_image_url { get; set; }
+    public object large_image_url { get; set; }
+    public object maximum_image_url { get; set; }
+}
+
+public class Aired
+{
+    public DateTime from { get; set; }
+    public object to { get; set; }
+    public Prop prop { get; set; }
+    public string _string { get; set; }
+}
+
+public class Prop
+{
+    public From from { get; set; }
+    public To to { get; set; }
+}
+
+public class From
+{
+    public int day { get; set; }
+    public int month { get; set; }
+    public int year { get; set; }
+}
+
+public class To
+{
+    public object day { get; set; }
+    public object month { get; set; }
+    public object year { get; set; }
+}
+
+public class Broadcast
+{
+    public object day { get; set; }
+    public object time { get; set; }
+    public object timezone { get; set; }
+    public object _string { get; set; }
+}
+
+public class Title
+{
+    public string type { get; set; }
+    public string title { get; set; }
+}
+
+public class Producer
+{
+    public int mal_id { get; set; }
+    public string type { get; set; }
+    public string name { get; set; }
+    public string url { get; set; }
+}
+
+public class Genre
+{
+    public int mal_id { get; set; }
+    public string type { get; set; }
+    public string name { get; set; }
+    public string url { get; set; }
+}
